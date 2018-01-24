e360c5ac962546f872d6d5f39e72f8e424ca8cd9
William Barbosa <william.sb@hotmail.com>
2018-01-24 17:45:59 -0200

    :construction: Add app specific back behaviour

---
+++ Toggl.Daneel.Tests.UI/AppExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.UITest;
+
+public static class AppExtensions
+{
+    public static void PerformBack(this IApp app, string identifier)
+    {
+        app.Tap(identifier);
+    }
+}
