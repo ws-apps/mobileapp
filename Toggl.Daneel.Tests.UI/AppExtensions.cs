a43496e94d4ff2d2115821ed92fdc461dbbffa49
William Barbosa <william.sb@hotmail.com>
2018-01-29 12:02:52 -0200

    :green_heart:

--- Toggl.Daneel.Tests.UI/AppExtensions.cs
+++ Toggl.Daneel.Tests.UI/AppExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using Xamarin.UITest;
+using Xamarin.UITest.Queries;
 
 public static class AppExtensions
 {
     public static void PerformBack(this IApp app, string identifier)
+    {
+        app.Tap(identifier);
+    }
+
+    public static void PerformBack(this IApp app, Func<AppQuery, AppQuery> identifier)
     {
         app.Tap(identifier);
     }
