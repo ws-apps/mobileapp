8e4df446ba6030641d1b3fc0be7bc37332e9b64e
Simon Rozsival <simon@rozsival.com>
2018-03-22 14:01:32 +0100

    :construction: Change to observable

--- Toggl.PrimeRadiant/Onboarding/DismissableOnboardingStep.cs
+++ Toggl.PrimeRadiant/Onboarding/DismissableOnboardingStep.cs
@@ -1,4 +1,7 @@
-﻿using Toggl.Multivac;
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Toggl.Multivac;
 using Toggl.PrimeRadiant.Settings;
 
 namespace Toggl.PrimeRadiant.Onboarding
@@ -7,9 +10,11 @@ namespace Toggl.PrimeRadiant.Onboarding
     {
         private readonly IOnboardingStep onboardingStep;
         private readonly IOnboardingStorage storage;
+        private readonly ISubject<bool> shouldBeVisibleSubject;
 
-        public bool ShouldBeVisible
-            => storage.WasDismissed(this) == false && onboardingStep.ShouldBeVisible;
+        private IDisposable internalSubscription;
+
+        public IObservable<bool> ShouldBeVisible { get; }
 
         public string Key { get; }
 
@@ -23,11 +28,19 @@ namespace Toggl.PrimeRadiant.Onboarding
             this.storage = storage;
 
             Key = key;
+
+            var wasAlreadyDismissed = storage.WasDismissed(this);
+            shouldBeVisibleSubject = new BehaviorSubject<bool>(wasAlreadyDismissed);
+            internalSubscription = onboardingStep.ShouldBeVisible.Subscribe(shouldBeVisibleSubject.OnNext);
+
+            ShouldBeVisible = shouldBeVisibleSubject.AsObservable();
         }
 
         public void Dismiss()
         {
             storage.Dismiss(this);
+            shouldBeVisibleSubject.OnNext(false);
+            shouldBeVisibleSubject.OnCompleted();
         }
     }
 }
